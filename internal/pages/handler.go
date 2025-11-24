2f2ef9ad671f8f44b48882e9dce6d381a31335d5
ShamsutOFF <shamsutoff@yandex.ru>
2025-11-24 12:32:38 +0300

    Добавил список карточек и хедер

--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -3,7 +3,8 @@ package pages
 import (
 	"github.com/gofiber/fiber/v2"
 	"news-fullstack/pkg/tadapter"
-	views "news-fullstack/views/components"
+	"news-fullstack/views"
+	"news-fullstack/views/types"
 )
 
 type PagesHandler struct {
@@ -20,7 +21,17 @@ func NewPagesHandler(router fiber.Router) {
 }
 
 func (h *PagesHandler) home(c *fiber.Ctx) error {
-	component := views.CategoryCard("Животные", "/public/images/animal_img.jpg")
+	categories := []types.Category{
+		{Name: "Еда", ImageURL: "/public/images/food_img.jpg"},
+		{Name: "Животные", ImageURL: "/public/images/animal_img.jpg"},
+		{Name: "Машины", ImageURL: "/public/images/car_img.jpg"},
+		{Name: "Спорт", ImageURL: "/public/images/sport_img.jpg"},
+		{Name: "Музыка", ImageURL: "/public/images/sport_img.jpg"},
+		{Name: "Технологии", ImageURL: "/public/images/tech_img.jpg"},
+		{Name: "Прочее", ImageURL: "/public/images/other_img.jpg"},
+	}
+
+	component := views.HomePage(categories)
 	return tadapter.Render(c, component)
 }
 
