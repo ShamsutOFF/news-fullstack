e2abaa68a7c5ccd301ff93d736b5a5f11f87e018
ShamsutOFF <shamsutoff@yandex.ru>
2025-12-04 09:44:55 +0300

    фикс

--- views/components/header_templ.go
+++ views/components/header_templ.go
@@ -41,7 +41,7 @@ func Header() templ.Component {
 		}
 		email := getUserEmail(ctx)
 		if email != "" {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div class=\"profile\" href=\"/users/logout\">Привет, ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<a class=\"profile\" href=\"/users/logout\">Привет, ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -54,7 +54,7 @@ func Header() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "! | <a href=\"/users/logout\">Выйти</a></div>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "!</a>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
